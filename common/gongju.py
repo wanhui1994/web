41573e31ab78cf750640932bb538863e0716aa64
wanhui1994 <2353231116@qq.com>
2018-04-16 17:39:09 +0800

    first commit

--- common/gongju.py
+++ common/gongju.py
@@ -1,12 +1,17 @@
 # coding=utf-8
-import xlrd,xlwt
+import pymysql
+import xlwt,xlrd
+class Mysql():
+    def mysql(self,ho,pr,us,pas,base):
+        db=pymysql.connect(host=ho,port=pr,user=us,password=pas,database=base)
+        cu=db.cursor()
+        sql="""SELECT * FROM `course` WHERE AGE=999"""
+        cu.execute(sql)
+        result=cu.fetchone() #查询的第一条结果输出
+        result1=cu.fetchall()  #查询的所有结果输出
+        a=cu.description()
 
-# 用来操作excel
-class Excel():
-    def __init__(self):
-        pass
 
-    #excel读
     def excelRead(self,path,name):
         wb=xlrd.open_workbook(path) #打开excel文件
         sh=wb.sheet_by_index(0) #通过sheet索引获取
@@ -15,11 +20,16 @@ class Excel():
         col_data = sh.col_values(0) #获取第X列的值
         row_data = sh.row_values(4) #获取第X行的值
 
-    #excel写
     def excelwrite(self,name,name1,path):
         wr=xlwt.Workbook(name)  #创建一个excel文件表
         sh=wr.add_sheet(name1)  #创建一个工作表对象
         sh.write(0, 0, 'EnglishName')   # 其中的'0-行, 0-列'指定表中的单元，'EnglishName'是向该单元写入的内容
         wr.save(path+name+'.xls')
-a=Excel()
-a.excelwrite('test','wh',"C:/Users/admin/Desktop/")
+
+    def mysqlwrite(self):   #搜索的mysql数据导入到一个excel
+
+
+
+
+a=Mysql()
+a.mysql("192.168.10.215",3306,"root","123456","testlibrary")
--- common/loginblog.py
+++ common/loginblog.py
@@ -1,1 +1,16 @@
 # coding=utf-8
+# import xlwt
+# i=0
+# wr=xlwt.Workbook('test')  #创建一个excel文件表
+# sh=wr.add_sheet('wh')  #创建一个工作表对象
+# while i<100:
+#     sh.write(i,0, 'wh1')   # 其中的'0-行, 0-列'指定表中的单元，'EnglishName'是向该单元写入的内容
+#     i+=1
+# wr.save(r'C:/Users/admin/Desktop/test.xls')
+import requests
+b=1
+while b<1000:
+    url="http://salegl.faxuan.net:83/saless/service/billService!doAddBillInvoiceInfo.do?address=%E5%8C%97%E4%BA%AC%E6%B5%8B%E8%AF%95&bankCode=NULL&bankName=%E5%8C%97%E4%BA%AC%E9%93%B6%E8%A1%8C&claimAdmin=&contactName=&email=ceshi%40faxuan.net&id=1000221&invoiceInfoDomainName=%E4%B8%87%E6%83%A0%E6%B5%8B%E8%AF%95%E5%8D%95%E4%BD%8D&invoiceType=0&mailAddress=&mobilePhone=13121322232&number=9999&operator=wanhui&price=999&subscribeId=1000149&taxpayersCode=2223332232323&telPhone=12306&titleType=4_0&total=999999"
+    a = requests.get(url)
+    print(a.text)
+    b=b+1
